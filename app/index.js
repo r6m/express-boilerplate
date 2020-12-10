6b88a3c3d76d80e2a49a7c49ca7c55ccc625211a
Reza Morsali <light.reza@gmail.com>
2020-12-10 14:01:08 +0330

    load dotenv

--- app/index.js
+++ app/index.js
@@ -1,12 +1,18 @@
 import express from "express";
 import { graphql } from './graphql';
+import cors from 'cors'
 import router from './router'
 import { config as loadConfig } from 'dotenv'
 
+const PORT = process.env.PORT || 4000
+
+loadConfig()
 require("./db/mongoose")
 
 const app = express();
 
+app.use(cors())
 app.use('/graphql', graphql)
+app.use('/', router)
 
-app.listen(4000, () => console.log('starting server on http://localhost:4000/graphql'))
+app.listen(PORT, () => console.log(`starting server on http://localhost:${PORT}`))
