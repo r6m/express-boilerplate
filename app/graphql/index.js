25cd228689c2b0a67bf84b46a7db9c4c46222faa
Reza Morsali <light.reza@gmail.com>
2020-12-24 19:10:07 +0330

    setup graphql shield

--- app/graphql/index.js
+++ app/graphql/index.js
@@ -2,9 +2,13 @@ import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
 import { graphqlHTTP } from 'express-graphql';
 import { NoSchemaIntrospectionCustomRule, specifiedRules } from 'graphql';
 import { loadFilesSync, makeExecutableSchema } from "graphql-tools";
+import { applyMiddleware } from 'graphql-middleware'
 import { join } from 'path';
-import { authDirective } from './directives/auth';
+import { User } from '../models';
 import resolvers from './resolvers';
+import { verifyToken } from '../utils/jwt';
+import { permissions } from './permissions'
+import { authDirective } from './directives/auth';
 
 const { authDirectiveTypeDefs, authDirectiveTransformer } = authDirective()
 
@@ -21,13 +25,42 @@ const schema = makeExecutableSchema({
   schemaTransforms: [authDirectiveTransformer],
 })
 
+const schemaWithMiddleware = applyMiddleware(
+  schema,
+  permissions
+)
+
 const productionOptions = {
   graphiql: false,
   validationRules: [...specifiedRules, NoSchemaIntrospectionCustomRule]
 }
-export const graphql = graphqlHTTP({
-  schema: schema,
-  graphiql: { headerEditorEnabled: true },
-  ...(process.env.NODE_ENV === 'production' ? productionOptions : {}),
-})
 
+const prepareContext = async (req) => {
+  const authorization = req.headers.authorization
+
+  if (authorization) {
+    const token = authorization.replace("Bearer ", "")
+    try {
+      const { sub } = verifyToken(token)
+      const user = await User.findById(sub)
+      return {
+        user: user,
+        userId: user?.id
+      }
+    } catch (error) {
+      console.log("graphql:prepareContext", error.message)
+    }
+  }
+  return {}
+}
+
+export const graphql = async (req, res, next) => {
+  const context = await prepareContext(req)
+
+  graphqlHTTP({
+    schema: schemaWithMiddleware,
+    graphiql: true,
+    context,
+    ...(process.env.NODE_ENV === 'production' ? productionOptions : {}),
+  })(req, res)
+}
